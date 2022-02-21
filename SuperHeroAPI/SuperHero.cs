b3d03984aae2a4556ac766bbf0f7e87e357eac6a
Gedeon Arruda <gd.deonnn@hotmail.com>
2022-02-21 16:57:03 -0300

    Add project files.

---
+++ SuperHeroAPI/SuperHero.cs
@@ -0,0 +1,12 @@
+﻿namespace SuperHeroAPI.Controllers
+{
+    public class SuperHero
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty ;
+        public string lastName { get; set; } = string .Empty ;
+        public string Place { get; set; } = string.Empty;
+    }
+}
+
